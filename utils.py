a26ec6a8e808d7f90d01aafd36dfcc3dc6724e79
toor <toor@macbook-air-13.local>
2024-10-10 15:47:33 +0400

    updated telethon

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
-from PIL import Image, ImageFont, ImageDraw
-from datetime import datetime
-from pytz import timezone
 import os
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+from pytz import timezone
 
 import config
 
 
 def get_current_time():
-    return datetime.now(timezone('Europe/Moscow')).strftime('%H:%M')
+    return datetime.now(timezone("Europe/Moscow")).strftime("%H:%M")
 
 
 def generate_image(text):
@@ -15,19 +16,45 @@ def generate_image(text):
     W, H = image.size
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype(font='resources/ds-digit.ttf', size=270)
+    font = ImageFont.truetype(font="resources/ds-digit.ttf", size=270)
     wt, ht = draw.textsize(text, font=font)
 
     border_size = config.image_border_size
     border_color = config.image_border_color
 
-    draw.text(((W - wt) / 2 - border_size, (H - ht) / 2 - border_size), text, font=font, fill=border_color)
-    draw.text(((W - wt) / 2 + border_size, (H - ht) / 2 - border_size), text, font=font, fill=border_color)
-    draw.text(((W - wt) / 2 - border_size, (H - ht) / 2 + border_size), text, font=font, fill=border_color)
-    draw.text(((W - wt) / 2 + border_size, (H - ht) / 2 + border_size), text, font=font, fill=border_color)
+    draw.text(
+        ((W - wt) / 2 - border_size, (H - ht) / 2 - border_size),
+        text,
+        font=font,
+        fill=border_color,
+    )
+    draw.text(
+        ((W - wt) / 2 + border_size, (H - ht) / 2 - border_size),
+        text,
+        font=font,
+        fill=border_color,
+    )
+    draw.text(
+        ((W - wt) / 2 - border_size, (H - ht) / 2 + border_size),
+        text,
+        font=font,
+        fill=border_color,
+    )
+    draw.text(
+        ((W - wt) / 2 + border_size, (H - ht) / 2 + border_size),
+        text,
+        font=font,
+        fill=border_color,
+    )
 
-    draw.text(((W - wt) / 2, (H - ht) / 2), text, font=font, fill=config.image_text_color)
+    draw.text(
+        ((W - wt) / 2, (H - ht) / 2),
+        text,
+        font=font,
+        fill=config.image_text_color,
+    )
 
+    image = image.convert("RGB")
     image.save(config.image_filename)
 
 
@@ -36,7 +63,7 @@ def delete_image():
 
 
 def get_progress_of_the_day():
-    now = datetime.now(timezone('Europe/Moscow'))
+    now = datetime.now(timezone("Europe/Moscow"))
     midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
     seconds = (now - midnight).seconds
     return round(seconds / 86400 * 100, 1)
